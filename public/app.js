c2e4e9f5254d5e238af0777337adfd1ffa67aeb3
TezukaKarlsefni <DjaluKun@gmail.com>
2023-02-16 14:03:47 +0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const root = document.querySelector('#root');
+function App() {
+  return /*#__PURE__*/React.createElement("h1", {
+    className: "news"
+  }, "Reactor");
+}
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+    const root = document.querySelector('#root');  
+
+    function App(){
+        return <h1 className='news'>Reactor</h1>
+    }
+
+    ReactDOM.render(<App/>, root);
